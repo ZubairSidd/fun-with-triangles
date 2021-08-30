cd66527fe97b3cd51915419b903c958b0015b350
Zubair Siddique <zubairsidd20@gmail.com>
2021-08-30 13:45:03 +0530

    quiz part done

---
+++ scripts/hypo.js
@@ -0,0 +1,6 @@
+const base = document.querySelector("#base");
+const height = document.querySelector("#height");
+const checkButton = document.querySelector("checkButton")
+const answer = document.querySelector("#answer")
+
+checkButton.
